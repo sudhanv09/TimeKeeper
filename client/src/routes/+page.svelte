1604d56bdf9a17136392845c09a2a90193e17488
sudhanv09 <mystogun125@gmail.com>
2023-10-15 11:22:04 +0530

    remove login button from home

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,15 +1,24 @@
 <script>
-	const currDate = new Date()
-	$: timeNow = currDate.toLocaleTimeString()
+	import { user } from '$lib/store';
+
+	const currDate = new Date();
+	$: timeNow = currDate.toLocaleTimeString();
 </script>
 
 <div class="flex flex-col space-y-10 h-screen justify-center items-center">
 	<div class="space-y-4">
 		<h1 class="text-5xl">{timeNow}</h1>
-		<h1 class="text-center">{currDate.toLocaleDateString("en-US", {weekday: "long"}) + "," + currDate.toLocaleDateString()}</h1>
+		<h1 class="text-center">
+			{currDate.toLocaleDateString('en-US', { weekday: 'long' }) +
+				',' +
+				currDate.toLocaleDateString()}
+		</h1>
 	</div>
 	<div class="space-x-6">
-		<a href="/login" class="btn variant-filled" data-sveltekit-preload-data="hover">Login</a>
-		<a href="/register" class="btn variant-filled" data-sveltekit-preload-data="hover">Register</a>
+		{#if !$user}
+			<a href="/login" class="btn variant-filled" data-sveltekit-preload-data="hover">Login</a>
+			<a href="/register" class="btn variant-filled" data-sveltekit-preload-data="hover">Register</a
+			>
+		{/if}
 	</div>
 </div>
